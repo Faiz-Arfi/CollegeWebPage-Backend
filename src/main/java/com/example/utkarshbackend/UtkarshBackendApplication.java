da32243765c68efaca23c1ef965a03ce42c78b2c
Faiz Arfi <faizarfi12@gmail.com>
2025-08-21 16:28:56 +0530

    initial project structure

---
+++ src/main/java/com/example/utkarshbackend/UtkarshBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.utkarshbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtkarshBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtkarshBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/utkarshbackend/UtkarshBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.utkarshbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtkarshBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
