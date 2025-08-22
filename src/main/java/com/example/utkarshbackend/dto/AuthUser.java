8af11692a3b23fe8b1f99f079950301652be8c57
Faiz Arfi <faizarfi12@gmail.com>
2025-08-22 21:19:18 +0530

    added builder pattern anotation

--- src/main/java/com/example/utkarshbackend/dto/AuthUser.java
+++ src/main/java/com/example/utkarshbackend/dto/AuthUser.java
@@ -1,12 +1,14 @@
 package com.example.utkarshbackend.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AuthUser {
     private Long id;
     private String email;
