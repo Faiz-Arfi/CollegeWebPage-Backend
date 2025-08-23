cc4943e1f40b192a22fb4aae3e0070acb0f468bb
Faiz Arfi <faizarfi12@gmail.com>
2025-08-23 16:52:18 +0530

    added methods to fetch teacher based on roles from db

--- src/main/java/com/example/utkarshbackend/repository/TeacherRepo.java
+++ src/main/java/com/example/utkarshbackend/repository/TeacherRepo.java
@@ -1,6 +1,9 @@
 package com.example.utkarshbackend.repository;
 
+import com.example.utkarshbackend.dto.TeacherDetailsDTO;
 import com.example.utkarshbackend.entity.Teacher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,10 @@ import java.util.Optional;
 @Repository
 public interface TeacherRepo extends JpaRepository<Teacher, Long> {
     Optional<Teacher> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Page<Teacher> getAllByRole(String hod, Pageable p);
+
+    Page<Teacher> findAllByRole(String teacher, Pageable p);
 }
